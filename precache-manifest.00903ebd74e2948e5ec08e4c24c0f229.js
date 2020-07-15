43a7a3f79e2ca8c5497d29120770946380f2b093
Muskaan Singh <muskaansingh235@gmail.com>
2020-07-15 17:20:44 +0530

    Updates

---
+++ precache-manifest.00903ebd74e2948e5ec08e4c24c0f229.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cfc8ccceed5b0933199e72f08cc1d2b",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "63708abaa664debed313",
+    "url": "/TodoList/static/css/main.7b9db43d.chunk.css"
+  },
+  {
+    "revision": "324e06252345d2cf917a",
+    "url": "/TodoList/static/js/2.b2f8a549.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/TodoList/static/js/2.b2f8a549.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63708abaa664debed313",
+    "url": "/TodoList/static/js/main.0a9f23b3.chunk.js"
+  },
+  {
+    "revision": "dd337711bee24ff7e940",
+    "url": "/TodoList/static/js/runtime-main.476b3ef9.js"
+  }
+]);
