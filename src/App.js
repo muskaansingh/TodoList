a4e0caf597e61f39683400d3e72327627f1f02ec
Muskaan Singh <muskaansingh235@gmail.com>
2020-08-06 13:01:39 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-//import UUID from 'react-uuid';
+import UUID from 'react-uuid';
 
 import './App.css';
 import Axios from 'axios';
@@ -8,34 +8,36 @@ import AddTodo from './Components/AddTodo';
 import About from './Components/pages/about'
 import Todos from './Components/Todo';
 import Header from './Components/Layouts/Header';
+
 // uuid is used to generate random id
 class App extends Component {
     state = {
-        todos: [ //{
-            //         id: UUID(),
-            //         title: 'Gyming',
-            //         completed: false
-            //     },
-            //     {
-            //         id: UUID(),
-            //         title: 'Webinar at 5 p.m',
-            //         completed: false
-            //     },
-            //     {
-            //         id: UUID(),
-            //         title: 'Meeting with boss',
-            //         completed: false
-            //     }
+        todos: [ {
+                    id: UUID(),
+                    title: 'Gyming',
+                    completed: false
+                },
+                {
+                    id: UUID(),
+                    title: 'Webinar at 5 p.m',
+                    completed: false
+                },
+                {
+                    id: UUID(),
+                    title: 'Meeting with boss',
+                    completed: false
+                }
         ]
     }
 
-    // fetch the todos from the api (jsonplaceholder)
-    componentDidMount() {
-        Axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
-            .then(res => this.setState({ todos: res.data }))
-    }
+    // // fetch the todos from the api (jsonplaceholder)
+    // componentDidMount() {
+    //     Axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    //         .then(res => this.setState({ todos: res.data }))
+    // }
 
     // cutting the completed task from the todo-list
+
     markComplete = (id) => {
         this.setState({
             todos: this.state.todos.map(todo => {
@@ -50,31 +52,23 @@ class App extends Component {
     //Deleting Todo-Item from the list
 
     delTodo = (id) => {
-        Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-            .then(res => this.setState({
-                todos: [...this.state.todos.filter(
-                    todo => todo.id !== id
-                )]
-            }));
-
-    }
+        this.setState({
+            todos: [...this.state.todos.filter(
+            todo => todo.id !== id)]
+            });
+     }
 
     // Add Todo
 
     addTodo = (title) => {
-        // const newTodo = {
-        //     id: UUID(),
-        //     title,
-        //     completed: false
-        // }
-        Axios.post('https://jsonplaceholder.typicode.com/todos', {
-                title,
-                completed: false
-            })
-            .then(res => this.setState({
-                todos: [...this.state.todos, res.data]
-            }))
-
+        const newTodo = {
+            id: UUID(),
+            title,
+            completed: false
+        }
+       this.setState({
+            todos: [...this.state.todos, newTodo]
+            });
     }
     render() {
 
